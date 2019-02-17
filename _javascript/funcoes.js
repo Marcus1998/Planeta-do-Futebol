7292679e9f6c32980aa4c2d52781cc0b50101e02
Marcus Teixeira <vbt.marcus@gmail.com>
2019-02-17 00:03:50 -0300

    foi inserido um sistema de seleção para o formato de visualização dos idolos e também foi iniciado o desenvolvimento do layout para um sistema de seleção para os videos da playlist na tela de multimidias

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,14 @@
-function mudaFoto(foto){
-		document.getElementById("icone").src = foto;
-}
+$(document).ready(function() {
+	$("img#todas").click(function(){
+		$("div.jcarousel-wrapper").hide(0, function(){
+			$("ul#album-fotos").show(0);
+		});
+		
+	});
+	
+	$("img#carrocel").click(function(){
+		$("ul#album-fotos").hide(0, function(){
+			$("div.jcarousel-wrapper").show(0);
+		});
+	});
+});
